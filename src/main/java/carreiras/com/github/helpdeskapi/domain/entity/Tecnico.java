12d3d072ce60326820b3ac1dd52abef703d2d6a9
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-09-01 12:07:39 -0300

    Entidades Chamado, Cliente, Pessoa, Tecnico

---
+++ src/main/java/carreiras/com/github/helpdeskapi/domain/entity/Tecnico.java
@@ -0,0 +1,34 @@
+package carreiras.com.github.helpdeskapi.domain.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa {
+
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+    }
+
+    public Tecnico(
+            Integer id,
+            String nome,
+            String cpf,
+            String email,
+            String senha) {
+        super(
+                id,
+                nome,
+                cpf,
+                email,
+                senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
